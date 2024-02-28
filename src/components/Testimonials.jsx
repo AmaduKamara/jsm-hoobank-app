084b94913480ac2323be00df42aa46e594d7420f
Amkam <amaduskamara@gmail.com>
2024-02-28 15:34:41 +0000

    Complete feedback styles

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { feedbacks } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+const Testimonials = () => (
+  <section id='clients' className={`${styles.flexCenter} flex-col relative`}>
+    {/* TODO */}
+    <div />
+
+    <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+      <h2 className={styles.heading2}>
+        What people are <br className='sm:block hidden' /> saying about us
+      </h2>
+      <div className='w-full md:mt-0 mt-6'>
+        <p className={`${styles.paragraph} max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+
+    <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+      {feedbacks.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
