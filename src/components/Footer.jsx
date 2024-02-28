cf0a3d73294e4fe5cb7ded67e9232e222499997d
Amkam <amaduskamara@gmail.com>
2024-02-28 16:39:31 +0000

    Complete Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,6 +41,24 @@ const Footer = () => (
         ))}
       </div>
     </div>
+
+    <div className='flex w-full justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45] '>
+      <p className='font-poppins font-normal text-center text-[16px] leading-[27px] text-white'>
+        Copyright &copy; 2024 HooBank. All Rights Reserved.
+      </p>
+      <div className='flex flex-row md:mt-0 mt-6'>
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+          ></img>
+        ))}
+      </div>
+    </div>
   </section>
 );
 
